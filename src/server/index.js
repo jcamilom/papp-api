7e7532e7c86e5e29349fc8361b31ed75e9d3dcd2
Camilo Muñoz <jcamilomunozz@gmail.com>
2017-12-13 00:49:59 -0500

    Index end

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,17 @@
-console.log("it works!");
+const Koa = require('koa');
+
+const app = new Koa();
+const PORT = 1337;
+
+app.use(async(ctx) => {
+    ctx.body = {
+        status: 'success',
+        message: 'papp api'
+    };
+});
+
+const server = app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+});
+
+module.exports = server;
